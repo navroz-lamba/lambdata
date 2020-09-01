c132034ab132ac8b945b6718e1dadae38e6c3031
Navroz Lamba <lamba-navroz@lambdastudents.com>
2020-09-01 07:58:00 +0530

    made an impuet that returns a dataframe

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # lambdata
 A Python package with helpful Data Science Utilities 
+
+------ 
+TODO:
+
+Adding a lot of valuable packages
---
+++ lambdata/__init__.py
@@ -0,0 +1,7 @@
+import pandas as pandas
+import numpy as np
+# libraries for the imputer module
+import sklearn
+
+
+TEST = pd.DataFrame(np.ones(10))
